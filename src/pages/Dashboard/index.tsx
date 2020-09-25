6de59d815633e4fe4df24c6be5decfe047c1b8bd
luiz-ortega <luizngust@hotmail.com>
2020-09-24 22:51:11 -0300

    feat: remove plate and update food list.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -72,7 +72,13 @@ const Dashboard: React.FC = () => {
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    try {
+      await api.delete(`/foods/${id}`);
+      const newFoods = foods.filter(food => food.id !== id);
+      setFoods([...newFoods]);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   function toggleModal(): void {
